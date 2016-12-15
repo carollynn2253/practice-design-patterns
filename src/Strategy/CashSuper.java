6c3df91be254640f219f4010f415106ee9a0bc69
ting <carollynn2253@gmail.com>
2016-12-15 12:29:08 +0800

    cashier use simple factory

---
+++ src/Strategy/CashSuper.java
@@ -0,0 +1,8 @@
+package Strategy;
+
+/**
+ * Created by chiu on 2016/12/15.
+ */
+public abstract class CashSuper {
+    public abstract int acceptCash(int originPrice);
+}
---
+++ src/Strategy/Cashier.java
@@ -0,0 +1,19 @@
+package Strategy;
+
+import java.util.Scanner;
+
+/**
+ * Created by chiu on 2016/12/15.
+ */
+public class Cashier {
+    public static void main(String[] args) {
+        System.out.print("Origin Total Price: ");
+        int originPrice = new Scanner(System.in).nextInt();
+
+        System.out.print("Please choose count type (1. normal  2. 20% off  3. 300 return 100): ");
+        int type = new Scanner(System.in).nextInt();
+
+        CashSuper cashSuper = CashFactory.createCashAccept(type);
+        System.out.print("Result Total: " + cashSuper.acceptCash(originPrice));
+    }
+}
